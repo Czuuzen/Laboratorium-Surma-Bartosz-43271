baf72fc2c78330c65691aafc06b4f4c22a2ced4e
Czuuzen <76943684+Czuuzen@users.noreply.github.com>
2021-01-26 21:23:08 +0100

    Add files via upload

---
+++ Lab zaliczeniowy/z1.py
@@ -0,0 +1,43 @@
+#zaliczka na PIT jest obliczana zgodnie z wzorem na rok 2017, reszta wartości jest aktualna
+def pit():
+    num4 = num1 - num2                              #num4 to różnica brutto i wartości ubezpieczeń społecznych (potrzebne do obliczenia pitu)
+    num5 = (round(num4, 2) - numx) * 0.18 - 46.33   #num5 to podstawa do obliczenia pitu
+    num6 = num5 - num4 * 0.0775                     #num6 to zaliczka na pit
+    return round(num6, 2)
+
+def netto():
+    num3 = num1 - (round(zdrowotne(num1, num2), 2) + num2 + pit())      #num3 to wartość netto
+    return round(num3, 2)
+
+def emerytalne(x):
+    return round(x * 0.0976, 2)
+
+def rentowe(x):
+    return round(x * 0.015, 2)
+
+def chorobowe(x):
+    return round(x * 0.0245, 2)
+
+def zdrowotne(x, y):
+    return round((x - y) * 0.09, 2)
+
+wybor1 = input("Czy pracujesz w miejscowości gdzie mieszkasz?(tak/nie): ")
+
+if wybor1 in ('tak', 'nie'):
+    if wybor1 == 'tak':
+        numx = 111.25
+    elif wybor1 == 'nie':
+        numx = 139.06
+
+if wybor1 in ('tak', 'nie'):
+    num1 = float(input("Podaj kwotę brutto: "))
+    num2 = emerytalne(num1)+rentowe(num1)+chorobowe(num1)       #num2 to wartość ubezpieczeń społecznych
+
+    print("Koszt ubezpieczenia emerytalnego", emerytalne(num1)," PLN")
+    print("Koszt ubezpieczenia rentowego", rentowe(num1)," PLN")
+    print("Koszt ubezpieczenia chorobowego", chorobowe(num1)," PLN")
+    print("Koszt ubezpieczenia zdrowotnego", zdrowotne(num1, num2)," PLN")
+    print("Zaliczka na PIT: ", pit(), " PLN")
+    print("Kwota w netto", netto(), " PLN")
+else:
+    print("Wybrałeś niepoprawną opcję")
---
+++ Lab zaliczeniowy/z2.py
@@ -0,0 +1,27 @@
+def emerytalne(x):
+    return round(x * 0.0976, 2)
+
+def rentowe(x):
+    return round(x * 0.065, 2)
+
+def wypadkowe(x):
+    return round(x * 0.0167, 2)
+
+def fundusz_pracy(x):
+    return round(x * 0.0245, 2)
+
+def fgsp(x):
+    return round(x * 0.001, 2)
+
+def lacznie():
+    y = emerytalne(num1) + rentowe(num1) + wypadkowe(num1) + fundusz_pracy(num1) + fgsp(num1)
+    return y
+num1 = float(input("Podaj kwotę brutto: "))
+
+print("Koszt ubezpieczenia emerytalnego", format(emerytalne(num1), '.2f')," PLN")
+print("Koszt ubezpieczenia rentowego", format(rentowe(num1), '.2f')," PLN")
+print("Koszt ubezpieczenia wypadkowego", format(wypadkowe(num1), '.2f')," PLN")
+print("Koszt składki na Fundusz Pracy", format(fundusz_pracy(num1), '.2f')," PLN")
+print("Koszt składki na Fundusz Gwarantowanych Świadczeń Pracowniczych: ", format(fgsp(num1), '.2f'), " PLN")
+print("Płaca pracownika wynosi: ", format((num1), '.2f')," PLN, opłaty na pracownika to: ", format(lacznie(), '.2f')," PLN,")
+print("a łączna kwota przeznaczona na pracownika to: ", lacznie() + num1," PLN.")
